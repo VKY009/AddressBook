9b3cdff8a00a27b6fbefea0b9cf4ef0502841e73
VKY009 <40037345+VKY009@users.noreply.github.com>
2024-01-03 18:51:07 +0530

    Refactored to add multiple Address Book to the System

--- src/main/java/AddressBook/AddressBookMain.java
+++ src/main/java/AddressBook/AddressBookMain.java
@@ -1,7 +1,5 @@
 package AddressBook;
-
 import java.util.Scanner;
-
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to the Multi-Address Book Program!");
